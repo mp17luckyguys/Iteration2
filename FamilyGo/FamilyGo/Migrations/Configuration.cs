438610500595045d7e7fb6c071b8144c2ea5fd55
jcui0003 <jcui0003@student.monash.edu>
2019-04-13 16:34:44 +1000

    new

---
+++ FamilyGo/FamilyGo/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace FamilyGo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FamilyGo.Models.familygogodbEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FamilyGo.Models.familygogodbEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
