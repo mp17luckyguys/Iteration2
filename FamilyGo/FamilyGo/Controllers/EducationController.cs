2917a139600899c1e3cc0d58d5d66c9fa2f8374d
Zekun Di <zdii0001@student.monash.edu>
2019-04-21 16:56:56 +1000

    function of language and country

---
+++ FamilyGo/FamilyGo/Controllers/EducationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyGo.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Country
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult GetJsonFromFile(string path)
+        {
+            try
+            {
+                string filepath = Server.MapPath(path);
+                string json = GetFileJson(filepath);
+                return Content(json);
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+        //具体读取json文件数据
+        public string GetFileJson(string filepath)
+        {
+            string json = string.Empty;
+            using (FileStream fs = new FileStream(filepath, FileMode.Open, System.IO.FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.GetEncoding("gb2312")))
+                {
+                    json = sr.ReadToEnd().ToString();
+                }
+            }
+            return json;
+        }
+    }
+}
